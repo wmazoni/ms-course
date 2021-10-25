01603b74fcbb1b948248a2d5ba000f18d0dbcfa2
Wellington Mazoni <wmazoni@protonmail.com>
2021-10-25 10:04:33 -0300

    Ribbon load balance

--- hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
@@ -1,10 +1,11 @@
 package com.wmazoni.hrworker.resources;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,11 @@ import com.wmazoni.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -29,6 +35,8 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
