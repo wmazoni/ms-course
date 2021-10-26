710974042016feb2f7073c6cbc3222b43105b21d
Wellington Mazoni <wmazoni@protonmail.com>
2021-10-26 19:53:21 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/wmazoni/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/wmazoni/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wmazoni.hrpayroll.entities.Payment;
 import com.wmazoni.hrpayroll.services.PaymentService;
 
@@ -17,9 +18,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = paymentService.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 }
