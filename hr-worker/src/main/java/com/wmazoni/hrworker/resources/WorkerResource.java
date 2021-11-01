6ff6efe73262c8cb8b392d8a8bc27b1808e7c47e
Wellington Mazoni <wmazoni@protonmail.com>
2021-11-01 18:09:03 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
@@ -18,30 +18,33 @@ import com.wmazoni.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
+
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerRepository workerRepository;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = workerRepository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
-		/*
-		 * try { Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
+
+		try {
+			Thread.sleep(3000L);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
+
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
