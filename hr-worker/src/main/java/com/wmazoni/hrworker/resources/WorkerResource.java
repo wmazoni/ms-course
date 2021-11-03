cb9ad72470622a5f6dfba4efbc67fdd4e72e5819
Wellington Mazoni <wmazoni@protonmail.com>
2021-11-03 14:15:17 -0300

    db config server

--- hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
@@ -23,9 +23,6 @@ import com.wmazoni.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
-	@Value("${test.config}")
-	private String testConfig;
 
 	@Autowired
 	private Environment env;
@@ -35,7 +32,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 
@@ -48,12 +45,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		try {
-			Thread.sleep(3000L);
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		/*
+		 * try { Thread.sleep(3000L);
+		 * 
+		 * } catch (InterruptedException e) { e.printStackTrace(); }
+		 */
 
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 
