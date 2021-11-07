05f772b6960b272d80a64fe8d9860e08ee9eb054
wmazoni <wmazoni@protonmail.com>
2021-11-06 21:56:31 -0300

    remoção de variavel de ambiente no projeto hr-worker

--- hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wmazoni/hrworker/resources/WorkerResource.java
@@ -24,8 +24,9 @@ public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
+	/*
+	 * @Value("${test.config}") private String testConfig;
+	 */
 
 	@Autowired
 	private Environment env;
@@ -35,7 +36,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 
