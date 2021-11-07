e0a00524f38806cb1a2d1023ff6ff39cac0ae421
wmazoni <wmazoni@protonmail.com>
2021-11-07 01:17:32 -0300

    removendo gateway zuul

--- hr-api-gateway-zuul/src/main/java/com/wmazoni/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/wmazoni/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -3,10 +3,9 @@ package com.wmazoni.hrapigatewayzuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
-@EnableZuulProxy
+//@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
