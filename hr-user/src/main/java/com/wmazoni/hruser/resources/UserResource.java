c535f05b563fa2534e97c690db51a57560a12f97
wmazoni <wmazoni@protonmail.com>
2021-11-07 14:43:11 -0300

    mostrar a porta da instencia no console

--- hr-user/src/main/java/com/wmazoni/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/wmazoni/hruser/resources/UserResource.java
@@ -1,6 +1,9 @@
 package com.wmazoni.hruser.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,18 +17,25 @@ import com.wmazoni.hruser.repositories.UserRepository;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserResource.class);
 
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		User obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}	
 
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findById(@RequestParam String email) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		User obj = repository.findByEmail(email);
 		return ResponseEntity.ok(obj);
 	}
